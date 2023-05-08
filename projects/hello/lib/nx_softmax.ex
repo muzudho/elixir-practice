90d9aed9b29e9693cb938b7ef9b9c0f748ee3246
むずでょ <muzudho1@gmail.com>
2023-05-09 00:00:12 +0900

    Update nx_softmax.ex

--- projects/hello/lib/nx_softmax.ex
+++ projects/hello/lib/nx_softmax.ex
@@ -11,6 +11,9 @@ defmodule Hello.Lib.NxSoftmax do
     `defn` は Nx.Defn からインポート
   """
   defn softmax(t) do
+    # exp(t) ... ネイピア数 e の t 乗
+    # sum(x) ... x の総和？
+    #
     Nx.exp(t) / Nx.sum(Nx.exp(t))
   end
 end
