bf86a3aa77396531b9512665a36344d27bbad8b1
むずでょ <muzudho1@gmail.com>
2023-05-08 21:07:03 +0900

    self_study

---
+++ self_study/hello.exs
@@ -0,0 +1,26 @@
+defmodule SelfStudy.Hello do
+  @moduledoc """
+  
+    ハロー
+  
+  ## (Examples)
+  
+    cd self_study
+    elixir hello.exs
+  """
+
+  @doc """
+  Loop.
+  
+  ## Examples
+  
+      iex> SelfStudy.Hello.do_1()
+  """
+  def do_1() do
+    IO.puts("hello")
+  end
+end
+
+## Script
+
+SelfStudy.Hello.do_1()
---
+++ self_study/nx.exs
@@ -0,0 +1,26 @@
+defmodule SelfStudy.Nx do
+  @moduledoc """
+  
+    Nx の練習
+  
+  ## (Examples)
+  
+    cd self_study
+    elixir nx.exs
+  """
+
+  @doc """
+  Loop.
+  
+  ## Examples
+  
+      iex> SelfStudy.Nx.do_1()
+  """
+  def do_1() do
+    IO.puts("hello")
+  end
+end
+
+## Script
+
+SelfStudy.Nx.do_1()
