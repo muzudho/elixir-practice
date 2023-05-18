8e15f31e3562176122bbeebc2a7859678acba233
むずでょ <muzudho1@gmail.com>
2023-05-18 22:39:15 +0900

    Create all.exs

---
+++ scripts/enum/all.exs
@@ -0,0 +1,28 @@
+# Run
+# ====
+#
+#   elixir all.exs
+#
+# Input
+# ======
+#
+#   [1, 2, 3]
+#   [1, nil, 3]
+#   []
+#
+# Output
+# ======
+#
+#   true
+#   false
+#   true
+#
+# References
+# ==========
+#
+#   📖 [Enum.all](https://hexdocs.pm/elixir/1.14.4/Enum.html#all?/1)
+#
+
+IO.puts(Enum.all?([1, 2, 3]))
+IO.puts(Enum.all?([1, nil, 3]))
+IO.puts(Enum.all?([]))
