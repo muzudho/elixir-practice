ea892ff07017a3493d75f03121236d6a71f28816
むずでょ <muzudho1@gmail.com>
2023-05-18 22:21:44 +0900

    enum sum

---
+++ scripts/recursive/enum_sum.exs
@@ -0,0 +1,23 @@
+# Run
+# ====
+#
+#   elixir enum_sum.exs
+#
+# Input
+# ======
+#
+#   1..10
+#
+# Output
+# ======
+#
+#   55
+#
+# References
+# ==========
+#
+#   📖 [Enum](https://hexdocs.pm/elixir/1.14.4/Enum.html)
+#
+
+# 1 から 10 までの数を全部足す
+IO.puts(Enum.sum(1..10))
