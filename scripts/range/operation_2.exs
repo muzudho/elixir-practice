c34941cc734144edb47d1b6332804a701c838c99
むずでょ <muzudho1@gmail.com>
2023-05-19 23:38:24 +0900

    Create operation_2.exs

---
+++ scripts/range/operation_2.exs
@@ -0,0 +1,31 @@
+# Run
+# ====
+#
+#   elixir operation_2.exs
+#
+# Input
+# ======
+#
+#   - `1..10` の要素数は？
+#   - `10` は、 1..10 のメンバーか？
+#   - `8` は、 1..10 のメンバーか？
+#
+# Output
+# ======
+#
+#   10
+#   false
+#   true
+#
+# References
+# ==========
+#
+#   📖 [Range](https://hexdocs.pm/elixir/1.14.4/Range.html)
+#
+
+# まず、 Range を定義する
+range = 1..10
+
+IO.puts(Enum.count(range))
+IO.puts(Enum.member?(range, 11))
+IO.puts(Enum.member?(range, 8))
